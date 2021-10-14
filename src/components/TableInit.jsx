c974f805d16ef8b465b0d05909bce37133d9a040
DESKTOP-N91C8ET\alekp <alekpawlowski21@gmail.com>
2021-10-14 19:28:33 +0200

    end for today

--- src/components/TableInit.jsx
+++ src/components/TableInit.jsx
@@ -3,20 +3,23 @@ import CharacterList from "./CharacterList.jsx";
 
 const TableInit = ({ importType }) => {
     const [error, setError] = useState(null);
-    const [isLoaded, setIsLoaded] = useState(false);
+    const [isLoaded = false, setIsLoaded] = useState(false);
     const [items, setItems] = useState([]);
     const fetchUrl = `https://rickandmortyapi.com/api/${importType}/`;
     console.log(fetchUrl, importType, isLoaded);
 
+    //use effect dosent work on rerun
     useEffect(() => {
         fetch(fetchUrl)
             .then((res) => res.json())
             .then(
                 (result) => {
+                    console.log(isLoaded);
                     setItems(result);
                     setIsLoaded(true);
                 },
                 (error) => {
+                    console.log(error);
                     setError(error);
                     setIsLoaded(false);
                 }
